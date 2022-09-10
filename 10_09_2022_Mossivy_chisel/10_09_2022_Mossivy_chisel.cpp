2cf3529f3ee13dc0e30f08bc6baca230a3b6fb64
hohlovscky2017 <hohlovskiido@gmail.com>
2022-09-10 18:27:48 +0300

    Написал код из занятия по МДК.04.03. Устройство и ункционирование информационной системы 10.09.2022

--- 10_09_2022_Mossivy_chisel/10_09_2022_Mossivy_chisel.cpp
+++ 10_09_2022_Mossivy_chisel/10_09_2022_Mossivy_chisel.cpp
@@ -1,11 +1,47 @@
 ﻿// 10_09_2022_Mossivy_chisel.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
+
+const int res_x = 15;
+const int res_y = 12;
+
+using namespace std;
 
 int main()
 {
+	std::srand(std::time(0));
 
+	int size_x, size_y, pos_x, pos_y;
+
+	size_x = 1 + rand() % res_x;
+	size_y = 1 + rand() % res_y;
+	pos_x = rand() % (res_x - size_x);
+	pos_y = rand() % (res_y - size_y);
+
+	int mass_pix[res_x][res_y];
+
+	for (int i = 0; i < res_x; i++)
+		for (int j = 0; j < res_y; j++) mass_pix[i][j] = rand() % 10;
+
+	for (int i = 0; i < res_x; i++) {
+		for (int j = 0; j < res_y; j++) cout << mass_pix[i][j] << " ";
+		cout << endl << endl;
+	}
+
+	for (int i = pos_x; i < pos_x + size_x; i++)
+		for (int j = pos_y; j < pos_y + size_y; j++) mass_pix[i][j] = 1;
+
+	cout << "///////////////////////////////////////////////////////////" << endl;
+
+	for (int i = 0; i < res_x; i++) {
+		for (int j = 0; j < res_y; j++) cout << mass_pix[i][j] << " ";
+		cout << endl << endl;
+	}
+
+	return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
